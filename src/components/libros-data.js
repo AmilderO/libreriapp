a28df3733b6324e2e5ed180009d9007152ecebca
AmilderO <aospinato@unal.edu.co>
2023-06-15 18:35:21 -0500

    Proyecto terminado?

---
+++ src/components/libros-data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        nombre: "Crimen y Castigo",
+        autor: "Fiódor Dostoyevski",
+        imagen: "https://global-uploads.webflow.com/6034d7d1f3e0f52c50b2adee/6254541d8ae4df16d4e69bc8_6034d7d1f3e0f54529b2b1a1_Crimen-y-castigo-fiodor-dostoyevski-editorial-alma.jpeg"
+    },
+    {
+        nombre: "El Gigante Enterrado",
+        autor: "Kazuo Ishiguro",
+        imagen: "https://www.anagrama-ed.es/uploads/media/portadas/0001/16/d3c3c1e162bed984b609d21bcde53b07977295e6.jpeg"
+    },
+    {
+        nombre: "1984",
+        autor: "George Orwell",
+        imagen: "https://imagessl4.casadellibro.com/a/l/t5/44/9788499890944.jpg"
+    },
+    {
+        nombre: "Don Quijote de la Mancha",
+        autor: "Miguel de Cervantes",
+        imagen: "https://www.mrbooks.com/mrbooks/portadas/9788497943529.webp"
+    },
+    {
+        nombre: "Orgullo y Prejuicio",
+        autor: "Jane Austen",
+        imagen: "https://ep00.epimg.net/elpais/imagenes/2013/01/28/fotorrelato/1359368956_663109_1359369063_album_normal.jpg"
+    },
+    {
+        nombre: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        imagen: "https://www.rae.es/sites/default/files/portada_cien_anos_de_soledad_0.jpg"
+    }
+];
